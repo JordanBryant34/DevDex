3515a923f2ad9aa457ae11a50e589b3c2761d085
JordanBryant34 <jordanbryant334@gmail.com>
2020-11-09 11:28:02 -0700

    added card collectionview cell file

---
+++ DevDexApp/DevDexApp/Views/CollectionView Cells/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  DevDexApp
+//
+//  Created by Jordan Bryant on 11/9/20.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+}
