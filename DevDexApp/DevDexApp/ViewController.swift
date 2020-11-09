32d5e2e690c9654c378ccf4c3e5f70367155001c
JordanBryant34 <jordanbryant334@gmail.com>
2020-11-09 11:05:31 -0700

    initial commit

---
+++ DevDexApp/DevDexApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevDexApp
+//
+//  Created by Jordan Bryant on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
